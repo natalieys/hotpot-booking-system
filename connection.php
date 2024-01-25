37ece8412f0263c395d96dc9e8190ff0c52dfc4a
natalieys <130364495+natalieys@users.noreply.github.com>
2024-01-25 15:35:59 +0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,16 +1,21 @@
 <?php
+
 $servername ="localhost";
 $username = "root";
 $password ="";
 $dbname="mydb";
+
 // Create connection
 $conn = mysqli_connect($servername,$username,$password,$dbname);
+
 // Check connection
-if (!$conn) {
+if (!$conn) 
+{
   die("Connection failed: " . mysqli_connect_error());
 }
 else
 {
 	echo "Sucessfully connected to database $dbname.";
 }
+
 ?>
